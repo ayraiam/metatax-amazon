cc7afde467668569d3042813392fed6285f2159e
Igor Lobo <97966076+oigorlobo@users.noreply.github.com>
2025-12-19 16:00:29 -0500

    Update r-base version from 4.3 to 4.5

--- workflow/run_downstream_analysis.sh
+++ workflow/run_downstream_analysis.sh
@@ -52,7 +52,7 @@ create_env() {
     else
       conda create -n "${ENV_NAME}" \
         -c conda-forge -c bioconda \
-        r-base=4.3 \
+        r-base=4.5 \
         r-data.table r-ggplot2 r-vegan r-tidyr r-dplyr r-stringr \
         r-cowplot r-rcolorbrewer r-ggbeeswarm \
         r-biocmanager \
